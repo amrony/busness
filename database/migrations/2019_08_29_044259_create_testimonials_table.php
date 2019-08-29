b11d209f8399a90b86250180c0ffc1ff806ded5e
amrony <aislamrony@gmail.com>
2019-08-29 11:52:51 +0600

    us

--- database/migrations/2019_08_29_044259_create_testimonials_table.php
+++ database/migrations/2019_08_29_044259_create_testimonials_table.php
@@ -15,6 +15,11 @@ class CreateTestimonialsTable extends Migration
     {
         Schema::create('testimonials', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('designation');
+            $table->text('image');
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
